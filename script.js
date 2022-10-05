bc09209a74fabf7bd33a94df8e33bf0386a234f1
nagyatl <00nagyattila@gmail.com>
2022-10-05 19:32:48 +0200

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById("homeButton").onclick = function () {
+    location.href = "home.html";
+};
+
+document.getElementById("tutorialButton").onclick = function () {
+    location.href = "tutorial.html";
+};
+
+document.getElementById("mainButton").onclick = function () {
+    location.href = "main.html";
+};
+
